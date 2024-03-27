c10f7edb9d444fd46c4dbebef422154ab712e8e7
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-27 16:42:05 +0100

    (js) create arrayImg

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Vue-hello',
-            Image: 'https://picsum.photos/200/300',
-            altimage: 'Picsum'
+            message: 'Vue-hello',            
+                
+            arrayImg: [
+                {
+                    img: 'https://picsum.photos/200/300',
+                    alt: 'Picsum'
+                }
+            ], 
         }
+    },
+    modules() {
+
     }
 }).mount('#app');
+
+
 
 
